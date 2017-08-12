4f944286662e78651097499dc83798c1863731f6
Brennan Brown <brennan.brown@edu.sait.ca>
2017-08-12 01:52:26 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,22 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+# Using the #each method with hashing.
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+# Here we are using #each to iterate over each pair of kids name/age. We are
+# yielding the key/value pair to the block of code between the do/end keywords
+# by assigning the variables kids_name and age in between the pipes, | |, to be
+# the placeholders for each key/value pair.
+
+# Then, we can use those variable names in our string interpolation to puts out
+# the actual values they point to at each step of the iteration.
+
+# Method takes in the argument of a hash that contains a name and a age as a
+# key/value pair respectively, iterating through each element to display 'Happy
+# Birthday' to each person.
+def happy_birthday(birthday)
+  # Using the #each method to iterate over each pair of name/age. The method
+  # yields the key/value pair to the block of code between the do/end keywords
+  # by assigning the variables name and age in between the pipes | | to be
+  # the placeholders for each key/value pair.
+  birthday.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
-
-
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,22 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+# Using the #each method with hashing.
 
+# Method takes in a hash of passengers as an argument,
+# iterates through the hash to determine the winner,
+# and returns the 'winner' value.
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  # Iterate through the hash using #each (not using #collect, due to how the
+  # method needs to identify the pair with the winner, and not collect the
+  # entire key/value.). With #each, the program grabs the winner's name and
+  # sets it equal to a variable to return.
+  passengers.each do |suite, name|
+    # Within the iteration, an if statement uses the && ("and") boolean
+    # operator to check if the element (passenger) has both the right suite and
+    # if the element (name) starts with "A".
+    if suite == :suite_a && name.start_with?("A")
+      # Assigns the name that meets both criteria as the winner.
+      winner = name
+    end
+  end
+  return winner
 end
