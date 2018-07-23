163914d9516fe0e461332cf43feb149b8c7b94bd
Roberto Garza <robertg4567@hotmail.com>
2018-07-23 14:30:36 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,24 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = "none"
+  passengers.each do |key, value|
+    if value.start_with?('A') && key.object_id == :suite_a.object_id
+      winner = value
+    end
+  end
+  winner
+end
+
+def happy_birthday(birthday_kids)
+                        #key, value
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
